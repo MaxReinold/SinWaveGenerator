9ef0a67fdc44273d4ac6e6de4d8ea55651b97728
StealthSov <50644368+StealthSov@users.noreply.github.com>
2021-03-09 17:10:34 -0500

    Added Description For Waves

--- script.js
+++ script.js
@@ -3,16 +3,25 @@ let ang = 0;
 
 function setup(){
   createCanvas(1880,1000);
+  waves.push(new Wave(40, 1500, 0, TWO_PI * 100));
   waves.push(new Wave(20, 3000, 0, TWO_PI * 100));
-  waves.push(new Wave(10, 5000, 0, TWO_PI * 100));
-  waves.push(new Wave(150, 100, 0, TWO_PI * 100));
+  waves.push(new Wave(10, 6000, 0, TWO_PI * 100));
 }
 
 function draw(){
   background(255);
   translate(width/2, height/2);
+  textSize(32);
+  for(let i = 0; i < waves.length; i++){
+    text("Wave "+ (i+1), -width/2, -450 + (50*3*i));
+    text("Amplitude: " + waves[i].amplitude, -width/2 + 40, -400+(50*3*i));
+    text("Period: " + waves[i].period, -width/2 + 40, -350+(50*3*i))
+  }
+
+  strokeWeight(.5);
+  ellipse(0,0,600,600);
   strokeWeight(2);
-  for(let a = 0; a < TWO_PI; a+=PI/180){
+  for(let a = 0; a < TWO_PI; a+=PI/720){
     let amp = 300 + waves[0].evaluate(a);
     for(let i = 1; i < waves.length; i++){
       amp+= waves[i].evaluate(a);
@@ -22,7 +31,7 @@ function draw(){
   }
 
   for(wave in waves){
-    waves[wave].phase+=.01;
+    waves[wave].phase+=.05;
   }
 }
 
